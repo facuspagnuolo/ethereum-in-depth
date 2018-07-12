8fa444670583b3bf703f94f9d4bc0fec138eee8d
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-07-12 10:51:24 -0300

    Fix calldata example

--- 3-data-management/contracts/Calldata.sol
+++ 3-data-management/contracts/Calldata.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.21;
 
 contract Calldata {
-  function tail(bytes32 text, uint256 from) public view returns (bytes32 tail) {
+  function tail(bytes32 text, uint256 offset) public view returns (bytes32 tail) {
     assembly {
       let ptr := mload(0x40)
-      let start := add(4, from)
-      let end := add(4, 32)
-      calldatacopy(ptr, start, end)
+      let start := add(4, offset)
+      let size := sub(add(4, 32), offset)
+      calldatacopy(ptr, start, size)
       tail := mload(ptr)
     }
   }
--- 3-data-management/test/Calldata.js
+++ 3-data-management/test/Calldata.js
@@ -8,7 +8,19 @@ contract('Calldata', () => {
     const rawResult = await calldata.tail(text, 6)
 
     const expectedString = text.substring(6)
-    const result = web3.toAscii(rawResult).replace(/\0/g, '')
+    const result = toAscii(rawResult)
     assert.equal(result, expectedString)
   })
+
+  // web3 toAscii function ignores the padding
+  function toAscii(hex) {
+    let str = '', i = 0, l = hex.length;
+    if (hex.substring(0, 2) === '0x') i = 2;
+    for (; i < l; i += 2) {
+      const code = parseInt(hex.substr(i, 2), 16);
+      if (code === 0) continue;
+      str += String.fromCharCode(code);
+    }
+    return str;
+  }
 })
